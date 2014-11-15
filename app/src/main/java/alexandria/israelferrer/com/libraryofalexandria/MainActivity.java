5aa5bc5c73bb834f29e8d963b870e0beb411f261
Israel Camacho <icamacho@twitter.com>
2014-11-15 15:47:55 -0800

    Construct presenter in the Activity and delegate onCreate

--- app/src/main/java/alexandria/israelferrer/com/libraryofalexandria/MainActivity.java
+++ app/src/main/java/alexandria/israelferrer/com/libraryofalexandria/MainActivity.java
@@ -17,13 +17,8 @@ import android.widget.ListView;
 import android.widget.RatingBar;
 import android.widget.TextView;
 
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
 import com.squareup.picasso.Picasso;
 
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.lang.reflect.Type;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.LinkedList;
@@ -43,8 +38,8 @@ public class MainActivity extends Activity {
 
         setContentView(R.layout.activity_main);
         ListView listView = (ListView) findViewById(R.id.listView);
-        
-
+        Presenter presenter = new ArtWorkListPresenter();
+        presenter.onCreate();
     }
 
     @Override
